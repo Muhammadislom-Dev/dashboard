bae241a685004eb9e8fcab318418f981574d10de
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-11-14 17:17:32 +0500

    add

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,49 +1,91 @@
 import React from "react";
 import "./Product.css";
 import image from "../asests/img/image.png";
+import axios from "axios";
 
 const Product = () => {
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    const formData = new FormData();
+    const { productName } = evt.target.elements;
+    const { amount } = evt.target.elements;
+    const { price } = evt.target.elements;
+    const { imageId } = evt.target.elements;
+    formData.append("productName", productName.value);
+    formData.append("amount", amount.value);
+    formData.append("price", price.value);
+    formData.append("imageId", imageId.value);
+
+    axios
+      .post(
+        "https://store-management-backend-app.herokuapp.com/api/v1/product",
+        formData
+      )
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="product">
       <div className="container">
         <h2 className="product-name">Create a new Product</h2>
-        <form action="" className="product-form">
+        <form onSubmit={handleSubmit} action="" className="product-form">
           <div className="product-title">
             <input
               required
               type="text"
               placeholder="Product name"
               className="product-input"
+              name="productName"
             />
-            <label style={{display:"flex",flexDirection:"column"}} htmlFor="file">
-                <p className="product-subname">Description</p>
+            <label
+              style={{ display: "flex", flexDirection: "column" }}
+              htmlFor="file"
+            >
+              <p className="product-subname">Description</p>
               <textarea
                 required
                 className="product-texts"
                 placeholder="Description"
               />
             </label>
-                <p className="product-subname">Add Images</p>
+            <p className="product-subname">Add Images</p>
             <label className="product-label" htmlFor="file">
               <img src={image} alt="" className="product-img" />
-              <input type="file" id="file" required className="product-file" />
+              <input
+                type="file"
+                name="imageId"
+                id="file"
+                required
+                className="product-file"
+              />
             </label>
           </div>
           <div className="product-list">
-              <input 
-              placeholder="Price" type="number" required className="product-number" />
-              <p style={{marginTop:"25px"}} className="product-subname">Select Category</p>
-              <select 
-            //   style={{margin:"15px 0"}}
-              className="product-select" name="" id="">
-                  <option value="First">First</option>
-                  <option value="Second">Second</option>
-                  <option value="Third">Third</option>
-              </select>
-              <input 
-              style={{marginTop:"25px"}}
-              type="number" required placeholder="Amount" className="product-number" />
-              <button className="product-button">Create Product</button>
+            <input
+              placeholder="Price"
+              type="number"
+              required
+              className="product-number"
+              name="price"
+            />
+            <p style={{ marginTop: "25px" }} className="product-subname">
+              Select Category
+            </p>
+            <select className="product-select" name="" id="">
+              <option value="First">First</option>
+              <option value="Second">Second</option>
+              <option value="Third">Third</option>
+            </select>
+            <input
+              style={{ marginTop: "25px" }}
+              type="number"
+              required
+              placeholder="Amount"
+              name="amount"
+              className="product-number"
+            />
+            <button className="product-button">Create Product</button>
           </div>
         </form>
       </div>
