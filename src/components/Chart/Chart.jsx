01a50e37772b5fb0d36a9d01047a725786336b5d
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-11-15 17:03:36 +0500

    add

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import './Chart.css'
+import img1 from '../asests/img/1.png'
+import img2 from '../asests/img/2.png'
+import img3 from '../asests/img/3.png'
+import img4 from '../asests/img/4.png'
 
 const Chart = () => {
   return (
     <div className='chart'>
         <div className="container">
             <h2 className="chart-name">Dashboard</h2>
+            
         </div>
     </div>
   )
